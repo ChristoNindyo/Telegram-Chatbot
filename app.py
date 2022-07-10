eb26b0a043e759d535825fc1d2b1ee479a01451e
Christopher Nindyo <105972495+ChristoNindyo@users.noreply.github.com>
2022-07-11 00:43:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def getMessage():
 def webhook():
    bot.remove_webhook()
    # TO DO: Edit the value of variable heroku_app_name according to your Heroku application name
-   heroku_app_name = "paseosmart"
+   heroku_app_name = "fast-hamlet-55034"
 
    # NOTE: You need to use a publically available URL that the Telegram servers can reach.
    bot.set_webhook(url=f'https://{heroku_app_name}.herokuapp.com/{TOKEN}')
